653f9848fba3e27fb36341586e3e6130b61a417f
GeoffreyPaul3 <geofreypaul40@gmail.com>
2024-10-10 16:41:46 +0200

    Fixed Main Nav

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -7,7 +7,7 @@ export function MainNav() {
   return (
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2">
-          <Image src="/tconnect.png" alt="" width={120} height={100}/>
+          <Image src="/tlogo.png" alt="" width={120} height={100}/>
       </Link>
     </div>
   )
